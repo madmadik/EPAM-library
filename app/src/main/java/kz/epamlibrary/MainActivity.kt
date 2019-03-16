40e4ac5f27918a5ad885e59125d03e231e3b0465
yeltayev <nurdaulet.97@gmail.com>
2019-03-16 18:23:33 +0600

    Add Firebase database

--- app/src/main/java/kz/epamlibrary/MainActivity.kt
+++ app/src/main/java/kz/epamlibrary/MainActivity.kt
@@ -4,6 +4,12 @@ import android.content.Context
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.ValueEventListener
 
 class MainActivity : AppCompatActivity() {
 
@@ -11,6 +17,27 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val database = FirebaseDatabase.getInstance()
+        val myRef = database.getReference("message")
+
+        myRef.setValue("Hello, World!")
+
+
+        // Read from the database
+        myRef.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                val value = dataSnapshot.getValue(String::class.java)
+                Log.d("YELTAYEV22", "Value is: " + value!!)
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                // Failed to read value
+                Log.w("YELTAYEV22", "Failed to read value.", error.toException())
+            }
+        })
 
         lateinit var intent: Intent
         val pref = getPreferences(Context.MODE_PRIVATE)
