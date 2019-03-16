6407c802fc252a3ef8622502f589272c4242c139
yeltayev <nurdaulet.97@gmail.com>
2019-03-16 23:40:17 +0600

    Add buttons

--- app/src/main/java/kz/epamlibrary/profile/mylibrary/EditActivity.kt
+++ app/src/main/java/kz/epamlibrary/profile/mylibrary/EditActivity.kt
@@ -2,6 +2,7 @@ package kz.epamlibrary.profile.mylibrary
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_edit.*
 import kz.epamlibrary.R
 
 class EditActivity : AppCompatActivity() {
@@ -9,5 +10,9 @@ class EditActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit)
+
+        back.setOnClickListener {
+            onBackPressed()
+        }
     }
 }
