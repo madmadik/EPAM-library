09e0cbfb5a0bacb18d92cc0db26a23a83f7cb0c9
yeltayev <nurdaulet.97@gmail.com>
2019-03-17 04:09:00 +0600

    add notifications fragment and ripple effect

--- app/src/main/java/kz/epamlibrary/notifications/NotificationsFragment.kt
+++ app/src/main/java/kz/epamlibrary/notifications/NotificationsFragment.kt
@@ -1,7 +1,16 @@
 package kz.epamlibrary.notifications
 
+import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import kz.epamlibrary.R
 
 class NotificationsFragment: Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val view = inflater.inflate(R.layout.fragment_notifications, container, false)
 
+        return view
+    }
 }
